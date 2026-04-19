7b8f029504501c459e79334f4a4d3adb6b214971
Nilusha_Madhuwanthi <nilushamadhuwanthi02@gmail.com>
2026-04-19 21:11:58 +0530

    Add resource not found exception

---
+++ Backend/PAF_Smart/src/main/java/com/tech/spcours/paf_smart/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.tech.spcours.paf_smart.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
