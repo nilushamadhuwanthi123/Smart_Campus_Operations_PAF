b3b8e3e982520dc754ffb85dc5ab35444d299d49
Nilusha_Madhuwanthi <nilushamadhuwanthi02@gmail.com>
2026-04-19 21:12:17 +0530

    Add unauthorized access exception

---
+++ Backend/PAF_Smart/src/main/java/com/tech/spcours/paf_smart/exception/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package com.tech.spcours.paf_smart.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
