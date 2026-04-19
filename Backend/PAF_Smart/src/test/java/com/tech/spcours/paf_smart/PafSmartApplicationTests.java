7fe7dfa5abd24794cf0fee5fcce20ea67382e5a2
Nilusha_Madhuwanthi <nilushamadhuwanthi02@gmail.com>
2026-04-19 21:32:54 +0530

    Add backend test class for Spring Boot application

---
+++ Backend/PAF_Smart/src/test/java/com/tech/spcours/paf_smart/PafSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.spcours.paf_smart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafSmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
