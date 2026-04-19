3b9bbdd0268503603f91e6a71cfeb8f1283b3007
Nilusha_Madhuwanthi <nilushamadhuwanthi02@gmail.com>
2026-04-19 21:11:21 +0530

    Add resource conflict exception

---
+++ Backend/PAF_Smart/src/main/java/com/tech/spcours/paf_smart/exception/ResourceConflictException.java
@@ -0,0 +1,8 @@
+package com.tech.spcours.paf_smart.exception;
+
+public class ResourceConflictException extends RuntimeException {
+
+    public ResourceConflictException(String message) {
+        super(message);
+    }
+}
