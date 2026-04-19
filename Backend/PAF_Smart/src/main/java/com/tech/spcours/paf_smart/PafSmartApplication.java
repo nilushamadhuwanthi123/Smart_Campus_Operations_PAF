1f2ac0621c15b2eae2d9cfbc5adf55fef1f47d10
Nilusha_Madhuwanthi <nilushamadhuwanthi02@gmail.com>
2026-04-19 21:14:41 +0530

    Add main Spring Boot application class

---
+++ Backend/PAF_Smart/src/main/java/com/tech/spcours/paf_smart/PafSmartApplication.java
@@ -0,0 +1,13 @@
+package com.tech.spcours.paf_smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafSmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PafSmartApplication.class, args);
+    }
+
+}
